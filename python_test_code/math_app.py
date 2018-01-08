69f7e44436c90de8f16bc8a8cd05964dddd2ad34
xiaobiky123 <hujie119121@163.com>
2018-01-08 13:05:34 +0800

    need5

--- python_test_code/math_app.py
+++ python_test_code/math_app.py
@@ -1,19 +1,18 @@
-def k_order_moment_about_the_origin(input_list,k):
+def k_order_origin_moment(input_list,k):
     summ=0
     lenth=len(input_list)
     for i in range(len(input_list)):
-        #summ=summ+k
         elem=input_list.pop()
         summ=summ+pow(elem,k)
         print elem
-    ju=float(summ)/float(lenth)
+    origin_moment=float(summ)/float(lenth)
     print "length=%d" %(lenth)
     print "summ=%f" % summ
-    print "ju=%f" % ju
-    return ju
+    print "origin_moment=%f" % origin_moment
+    return origin_moment
 
 input_list=[1050, 1100,	1080,	1120,	1200,	1250,	1040,	1130,	1300, 1200]        
-output=k_order_moment_about_the_origin(input_list,1)
+output=k_order_origin_moment(input_list,1)
 print "the_k_order_moment_about_the_origin = %f" %output
 
 
@@ -22,24 +21,18 @@ def k_order_central_moment(input_list,k):
     summ_a=0
     lenth=len(input_list)
     for i in input_list:
-        #summ=summ+k
-        #elem=input_list.pop()
         summ_a=summ_a+i
-        #print elem
     ave=float(summ_a)/float(lenth)
 
     for j in input_list:
-        #summ=summ+k
-        #elem=input_list.pop()
         summ=summ+pow((j-ave),2)
-        #print elem
-    cent_mom=float(summ)/float(lenth)
+    central_moment=float(summ)/float(lenth)
 
     print "length=%d" %(lenth)
     print "summ=%f" % summ
-    print "ju=%f" %  cent_mom
+    print "central_moment=%f" %  central_moment
     print "ave=%f" % ave
-    return  cent_mom
+    return  central_moment
 
 input_list=[1050, 1100,	1080,	1120,	1200,	1250,	1040,	1130,	1300, 1200]        
 output=k_order_central_moment(input_list,2)
